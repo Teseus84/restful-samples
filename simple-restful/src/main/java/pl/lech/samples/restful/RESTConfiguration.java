57e9e185af6370e8cdec90f52698d006e6b9ee25
Teseus84 <lech.szewczyk@rec-offshore.intranet>
2016-04-14 20:28:24 +0200

    deleting old folder

--- simple-restful/src/main/java/pl/lech/samples/restful/RESTConfiguration.java
+++
@@ -1,9 +0,0 @@
-package pl.lech.samples.restful;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class RESTConfiguration extends Application {
-
-}
