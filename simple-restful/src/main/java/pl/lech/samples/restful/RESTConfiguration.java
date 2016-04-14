cfbb7d72d851a1e74b984b00c230ac1643f8ac77
Teseus84 <lech.szewczyk@rec-offshore.intranet>
2016-04-14 13:42:47 +0200

    Small formatting changes

--- simple-restful/src/main/java/pl/lech/samples/restful/RESTConfiguration.java
+++ simple-restful/src/main/java/pl/lech/samples/restful/RESTConfiguration.java
@@ -4,6 +4,6 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("resources")
-public class RESTConfiguration extends Application{
+public class RESTConfiguration extends Application {
 
 }
