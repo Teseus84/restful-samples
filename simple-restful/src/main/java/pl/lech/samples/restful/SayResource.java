1ef9475bcd6e29db45e11fe04f8d27e912f0ea52
Teseus84 <lech.szewczyk@rec-offshore.intranet>
2016-04-14 15:19:02 +0200

    Added return type

--- simple-restful/src/main/java/pl/lech/samples/restful/SayResource.java
+++ simple-restful/src/main/java/pl/lech/samples/restful/SayResource.java
@@ -3,17 +3,20 @@ package pl.lech.samples.restful;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 @Path("say")
 public class SayResource {
 
 	@GET
+	@Produces("text/html")
 	public String sayHello() {
 		return "Hello World";
 	}
 
 	@GET
 	@Path("/{text}")
+	@Produces("text/html")
 	public String saySomething(@PathParam("text") String text) {
 		return "Hello " + text;
 	}
